4d44a3308da2cfe7a957aa709b8bf807aed29b84
Fvaldenegro95 <fvaldenegro95@gmail.com>
2021-11-09 20:46:39 -0500

    1

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import Friends from './components/Friends';
+import PrivateRoute from './components/PrivateRoute';
 
 
-const Login = ()=> {
-  return (<h2>Login</h2>)
-}
 
 function App() {
+  const isLoggedIn = localStorage.getItem('token')
   return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
+
+        <Router>
+        <div className="App">
+  
+          <h2>Client Auth Project</h2>
+  
+          <ul>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+            <li>
+              <Link to='/logout'>Logout</Link>
+            </li>
+            <li>
+              {isLoggedIn && <Link to='/friends'>Friends</Link>}
+            </li>
+          </ul>
+  
+          <Switch>
+            <PrivateRoute exact path='/friends' component={Friends}/>
+            <Route path='/logout' component={Logout}/>
+            <Route path='/login' component={Login}/>
+            <Route path='/' component={Login}/>
+          </Switch>
+  
+        </div>
+      </Router>
   );
 }
 
---
+++ friends/src/components/Logout.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import { useHistory } from 'react-router-dom';
+import axiosWithAuth from "../utilities/axiosWithAuth";
+
+const Logout = () => {
+    const {push} = useHistory();
+
+    useEffect(()=> {
+        axiosWithAuth()
+            .post('http://localhost:5000/api/logout')
+            .then(response => {
+                localStorage.removeItem('token');
+                push('/login');
+            });
+    }, []);
+
+    return(<div></div>);
+}
+
+export default Logout; 
